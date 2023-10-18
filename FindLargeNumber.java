a9916b4358eb571a19067c8b0da0d0e6c4f6c45c
Pavan Bangera <92009490+pavanbangera@users.noreply.github.com>
2023-10-18 17:10:43 +0530

    finding largest number from user given array

--- FindLargeNumber.java
+++ FindLargeNumber.java
@@ -1,3 +1,28 @@
+import java.util.Scanner;
+
 public class FindLargeNumber {
+    public static void main(String[] args) {
+        System.out.print("Enter the Length of array: ");
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        System.out.println();
+        System.out.print("Enter the number: ");
+        int arr[] = new int[n];
+        int i = 0;
+        while (i < n) {
+            arr[i] = in.nextInt();
+            i++;
+        }
+        int max = Integer.MIN_VALUE;
+        i = 0;
+        while (i < arr.length) {
+            if (max < arr[i]) {
+                max = arr[i];
+
+            }
+            i++;
+        }
+        System.out.println("Largest number is: " + max);
+    }
 
 }
